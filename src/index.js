072567f8b2afd326d1391a724d6000f080d7f94b
Sergey Tugaenko <stugaenko@qualityunit.com>
2022-01-01 14:00:24 +0200

    From input event to ajax request through piping

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import {fromEvent, mergeMap} from 'rxjs';
+import {map, distinctUntilChanged, debounceTime, switchMap} from 'rxjs/operators';
+import {ajax} from 'rxjs/ajax';
+
+const url = 'https://api.github.com/search/users?q=';
+
+//Get input element from index.html
+const search = document.getElementById('search');
+
+//create observable from input event
+const stream$ = fromEvent(search, 'input')
+    .pipe(
+        //Get input value from event
+        map(e => {
+            return e.target.value;
+        }),
+        //Get last cached emmit data after debounce time
+        debounceTime(1000),
+        //Emit only distinct changes
+        distinctUntilChanged(),
+        //Create new observable from exist. INPUT STRING to AJAX RESPONSE
+        switchMap(v => ajax.getJSON(url + v)),
+        //Get only necessary data from response
+        map(response => response.items),
+        //Create by one subscribe for every item. Every item is separate observable now
+        mergeMap(items => items)
+    );
+
+stream$.subscribe(value => {
+    console.log(value);
+})
